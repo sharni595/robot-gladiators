c4fe4f5909055c69cc8b92c3641eee8807d31a9f
Sharni Zaugg <sharni.greener@gmail.com>
2021-03-20 21:38:26 -0600

    created round one of robot fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,27 @@
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
-
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("This logs a string, good for leaving yourself a message");
+var fight = function(){
+    // alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-console.log(10 + 10);
+    //subtract the value of 'playerAttack' from the value of 'enemyHealth'
+    enemyHealth = enemyHealth - playerAttack;
+    //log a resulting message to the console so we know it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+    //subtract the value of 'enemyAttack' from the value of 'playerHealth'
+    playerHealth = playerHealth - enemyAttack;
+    //,log a resulting message to the console so we know it worked
+    enemyName + " attacked " + playerName + ". " + playerName + " now has" + playerHealth + " health remaining."
+};
 
-console.log("Our robot's name is " + playerName);
-
-//fight();
+fight();
