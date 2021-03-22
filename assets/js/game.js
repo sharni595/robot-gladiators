4c06407ed3a97cb17b406da4d4438b088866b75c
Sharni Zaugg <sharni.greener@gmail.com>
2021-03-21 22:37:47 -0600

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,6 +75,9 @@ var fight = function(enemyName){
 };
 
 for(var i = 0; i < enemyNames.length; i++){
+    if (playerHealth > 0){
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+    }
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
