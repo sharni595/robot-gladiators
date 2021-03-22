15f68cb6f6ed5c8b428f4a89443a04f8179a16d1
Sharni Zaugg <sharni.greener@gmail.com>
2021-03-21 21:33:21 -0600

    enemy combatants added

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,16 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){
+/* for([initial expression]; [condition]; [increment expression]) {
+    statement
+ } */
+
+
+var fight = function(enemyName){
     // alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -64,8 +67,8 @@ var fight = function(){
     } else {
         window.alert("You need to choose a valid option. Try again!")
     }
-
-
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
