3b79adc06cba69b578e8224a7121507b993467bb
Sharni Zaugg <sharni.greener@gmail.com>
2021-03-25 20:16:30 -0600

    make shop functions more efficient with integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -123,19 +123,17 @@ var endGame = function(){
 //shop
 var shop = function(){
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to 1. REFILL your health, 2. UPGRADE your attack, or 3. LEAVE the store? Please enter '1', '2', or '3' to make a choice."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt){
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
            playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             break;
         default:
