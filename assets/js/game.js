140b4b0fbce21cebf424e5b156b99e886a8a1434
Sharni Zaugg <sharni.greener@gmail.com>
2021-03-25 18:48:48 -0600

    fix bug that allows player to continue without inputting name

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,8 +149,18 @@ var randomNumber = function(min, max){
     return value;
 };
 
+var getPlayerName = function(){
+    var name = "";
+    while (name === "" || name === null){
+       name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+    
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10, 
@@ -174,7 +184,7 @@ var playerInfo = {
             this.attack += 6;
             this.money -= 7;
         } else{
-            window.alert("You don't have anout money!");
+            window.alert("You don't have enough money!");
         }
     }
 };
